f8592383d52b85fc8daa000d984f1abd2fcad235
Levavft <Levavft@gmail.com>
2022-12-01 01:06:41 +0900

    sumdirectedgraph fails badly, following a path doesn't gaurantee zero's at all! because it only checks if adding one element at a time created a zero - it doesn't account for reordering.
    possible solution - when creating the graph, reorder stuff in all sorts of ways. also remove zero paths in this new weird reorder graph.
    this is more for me than any future reader, I'm aware of how incomprehensible this is.

--- test.py
+++ test.py
@@ -2,18 +2,31 @@ import unittest
 from datetime import datetime
 from main import memoized_calculate_v
 from abeliangroup import group_values
+from sumdirectedgraph import SumDiGraph
 
 
 class MyTestCase(unittest.TestCase):
-    def test_validity(self):
+    def test_validity_main_code(self):
         f = memoized_calculate_v
         for g, v in group_values.items():
             with self.subTest(function=f, group=g, value=v):
-                info_string = f"\nfunction: {f.__name__}\ngroup: {g}\nresult: {v}"
+                info_string = f"\nfunction: {f.__name__}\ngroup: {g}\nexpected: {v}"
+                print(f"\nstarting: {info_string}")
+                t = datetime.now()
+                result = f(g)
+                self.assertEqual(result, v, msg=info_string)
+                print(f"result: {result}\ntook: {datetime.now() - t}")
+
+    def test_validity_sum_directed_graph(self):
+        for g, v in group_values.items():
+            with self.subTest(group=g, value=v):
+                info_string = f"\ngroup: {g}\nexpected: {v}"
                 print(f"\n\nstarting: {info_string}")
                 t = datetime.now()
-                self.assertEqual(f(g), v, msg=info_string)
-                print(f"took: {datetime.now() - t}")
+                s = SumDiGraph(g)
+                result = s.calculate_v()
+                self.assertEqual(result, v, msg=info_string)
+                print(f"result: {result}\ntook: {datetime.now() - t}")
 
 
 if __name__ == '__main__':
