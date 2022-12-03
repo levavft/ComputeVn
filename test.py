a8503b9e57efc5203840ba131138921e0417a283
Levavft <Levavft@gmail.com>
2022-12-03 18:59:15 +0900

    delete sumdirectedgraph.py

--- test.py
+++ test.py
@@ -17,17 +17,6 @@ class MyTestCase(unittest.TestCase):
                 self.assertEqual(result, v, msg=info_string)
                 print(f"result: {result}\ntook: {datetime.now() - t}")
 
-    # def test_validity_sum_directed_graph(self):
-    #     for g, v in group_values.items():
-    #         with self.subTest(group=g, value=v):
-    #             info_string = f"\ngroup: {g}\nexpected: {v}"
-    #             print(f"\n\nstarting: {info_string}")
-    #             t = datetime.now()
-    #             s = SumDiGraph(g)
-    #             result = s.calculate_v()
-    #             self.assertEqual(result, v, msg=info_string)
-    #             print(f"result: {result}\ntook: {datetime.now() - t}")
-
 
 if __name__ == '__main__':
     unittest.main()
