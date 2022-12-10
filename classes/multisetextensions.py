5527a23b9967306021785f9b1c5a3411e0d26ee3
Levavft <Levavft@gmail.com>
2022-12-10 18:06:27 +0900

    None

--- classes/multisetextensions.py
+++ classes/multisetextensions.py
@@ -1,15 +1,18 @@
 from multiset import Multiset
 
+
 class SummableMultiset(Multiset):
-    def __init__(self, em = []):
+    def __init__(self, em=None):
+        if em is None:
+            em = []
         self.tracked_sum = sum(em)
         super().__init__(em)
 
-    def add(self, e):
+    def add(self, e, multiplicity=1):
         #print(self.tracked_sum, e)
         self.tracked_sum = self.tracked_sum + e
         #print(self.tracked_sum, e)
-        super().add(e)
+        super().add(e, multiplicity=multiplicity)
 
     def get_sum(self):
         return self.tracked_sum
